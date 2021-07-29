d03d2aab7c45f418ebc41566f80fe5e087b36a9c
Sebastián M <smunozmo@gmail.com>
2021-07-28 23:30:39 -0500

    Add API call.

--- src/index.js
+++ src/index.js
@@ -2,3 +2,81 @@
 
 import _ from 'lodash';
 import './style.css';
+
+const refreshBtn = document.getElementById('refresh');
+const submitBtn = document.getElementById('add');
+const leaderboard = document.getElementById('leaderboard');
+const nameInput = document.getElementById('name');
+const scoreInput = document.getElementById('score');
+
+function addScore() {
+
+    fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/mchCFxyMLOw3tEZP2NKf/scores/', {
+      method: 'POST',
+      body: JSON.stringify({
+          user: nameInput.value,
+          score: scoreInput.value
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    })
+      .then((response) => response.json())
+      .then((json) => console.log(json));
+}
+
+function createBoard(obj) {
+    leaderboard.innerHTML = '';
+    obj.result.forEach((e, index) => {
+        leaderboard.innerHTML += `<li class="list-group-item">${obj.result[index].user} ${obj.result[index].score}</li>`
+    });
+}
+
+async function getScores() {
+    const getData = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/mchCFxyMLOw3tEZP2NKf/scores/');
+    const usersAndScores = await getData.json();
+    createBoard(usersAndScores);
+    return usersAndScores;
+}
+
+getScores();
+
+refreshBtn.addEventListener('click', getScores)
+submitBtn.addEventListener('click', addScore)
+
+
+
+
+// fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/', {
+//     method: 'POST',
+//     body: JSON.stringify({
+//       name: 'smunozmo'
+//     }),
+//     headers: {
+//       'Content-type': 'application/json; charset=UTF-8',
+//     },
+//   })
+//     .then((response) => response.json())
+//     .then((json) => console.log(json));
+
+   // Game with ID: mchCFxyMLOw3tEZP2NKf added.
+
+//    fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/mchCFxyMLOw3tEZP2NKf/scores/', {
+//     method: 'POST',
+//     body: JSON.stringify({
+//         user: "John Doe",
+//         score: 42
+//     }),
+//     headers: {
+//       'Content-type': 'application/json; charset=UTF-8',
+//     },
+//   })
+//     .then((response) => response.json())
+//     .then((json) => console.log(json));
+
+// async function showLeaderboard() {
+//     const scores = await getScores();  
+//     console.log(scores);
+    
+// }
+
